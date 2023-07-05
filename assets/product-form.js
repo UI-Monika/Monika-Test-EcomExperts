93c121bd52f087d64d8fd3b0be0fe1d59e29e9b1
UI-Monika <109434700+UI-Monika@users.noreply.github.com>
2023-07-05 20:25:31 +0530

    check

--- assets/product-form.js
+++ assets/product-form.js
@@ -19,28 +19,7 @@ if (!customElements.get('product-form')) {
         evt.preventDefault();
         if (this.submitButton.getAttribute('aria-disabled') === 'true') return;
 
-        const variantID = document.querySelector(".product-variant-id").value;
-        const quantityInput = document.querySelector(".quantity__input").value;
         
-        setTimeout(()=>{
-        if(variantID == "45521462722843"){
-         let formData = {
-           'items': [{
-            'id': 45513239691547,
-            'quantity': quantityInput,
-            }]
-          };
-          fetch(window.Shopify.routes.root + 'cart/add.js', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-          })
-          .catch((error) => {
-            console.error('Error:', error);
-          });
-        }},500);
 
         this.handleErrorMessage();
 
@@ -116,12 +95,7 @@ if (!customElements.get('product-form')) {
             this.querySelector('.loading-overlay__spinner').classList.add('hidden');
           });
 
-          
-        
-  
-
-      
-        }
+         }
 
       handleErrorMessage(errorMessage = false) {
         if (this.hideErrors) return;
